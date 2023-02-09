1513667375f21921ecf3dea619ed76900f135b61
Eve-Kamau <evelyne.kamau@student.moringaschool.com>
2023-02-09 03:40:17 +0300

    Completed Lab

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,23 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+ 
+  const pageLinks=links.map(link => < a key={link} href={link}>{link}</a>);
+  console.log(pageLinks);
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+     <nav>
+      {pageLinks}
+    </nav>
+   );
 }
-
 export default NavBar;
+
+
+ //{/* display an <a> tag for each link here */}
+
+  // {/* <a key="name" href="#home">home</a>
+    //<a key="name" href="#about">about</a>
+    //<a key="name" href="#projects">projects</a>
+
+  
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -2,13 +2,27 @@ import React from "react";
 import ProjectItem from "./ProjectItem";
 
 function ProjectList({ projects }) {
-  console.log(projects);
-  return (
+  // console.log(projects)
+
+  const projectItems = projects.map(project => <li key={project.id} > {project.name} </li>);
+  // console.log(projectItems)
+
+  const techList = projects.map(technology => {
+  return(
+  <ProjectItem key={technology.name} name={technology.name} about={technology.about} technologies={technology.technologies} />)
+  })
+  // console.log(techList);
+
+  return(
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list"> {/* render ProjectItem components here */}
+      {/* {projectItems} */}
+      {techList}
+      </div>
     </div>
   );
 }
 
 export default ProjectList;
+
